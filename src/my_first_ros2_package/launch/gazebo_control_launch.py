8e7b6426f26a3dec657ea8325a6d3641d1f02208
MinhDuc Tran <ductm.tran@gmail.com>
2026-09-20 19:03:38 -0400

    Automate Gazebo ros2_control simulation launch

---
+++ src/my_first_ros2_package/launch/gazebo_control_launch.py
@@ -0,0 +1,100 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name = 'my_first_ros2_package'
+
+    package_share = get_package_share_directory(package_name)
+
+    urdf_file = os.path.join(
+        package_share,
+        'urdf',
+        'palletizing_robot.urdf'
+    )
+
+    with open(urdf_file, 'r') as file:
+        robot_description = file.read()
+
+    # Start Gazebo
+    gazebo = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', 'empty.sdf'],
+        output='screen'
+    )
+
+    # Publish robot_description and TF
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {
+                'robot_description': robot_description,
+                'use_sim_time': True
+            }
+        ]
+    )
+
+    # Spawn robot into Gazebo
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-world', 'empty',
+            '-file', urdf_file,
+            '-name', 'palletizing_robot',
+            '-x', '0',
+            '-y', '0',
+            '-z', '0'
+        ],
+        output='screen'
+    )
+
+    # Bridge Gazebo simulation clock to ROS 2
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
+        ],
+        output='screen'
+    )
+
+    # Load + configure + activate joint_state_broadcaster
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--controller-manager',
+            '/controller_manager'
+        ],
+        output='screen'
+    )
+
+    # Load + configure + activate arm_controller
+    arm_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'arm_controller',
+            '--controller-manager',
+            '/controller_manager'
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+        spawn_robot,
+        clock_bridge,
+        joint_state_broadcaster,
+        arm_controller,
+    ])
--- src/my_first_ros2_package/setup.py
+++ src/my_first_ros2_package/setup.py
@@ -7,21 +7,44 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # THÊM DÒNG DƯỚI ĐÂY ĐỂ ĐƯA FILE LAUNCH VÀO HỆ THỐNG
-        ('share/' + package_name + '/launch', ['launch/my_nodes_launch.py']),
-        ('share/' + package_name + '/config', ['config/params.yaml']),
-        ('share/' + package_name + '/urdf', ['urdf/palletizing_robot.urdf']),
-        ('share/' + package_name + '/rviz', ['rviz/robot_config.rviz']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            'share/' + package_name + '/launch',
+            [
+                'launch/my_nodes_launch.py',
+                'launch/gazebo_control_launch.py',
+            ]
+        ),
+        (
+            'share/' + package_name + '/config',
+            [
+                'config/params.yaml',
+                'config/controllers.yaml',
+                'config/arm_controller.yaml',
+            ]
+        ),
+        (
+            'share/' + package_name + '/urdf',
+            ['urdf/palletizing_robot.urdf']
+        ),
+        (
+            'share/' + package_name + '/rviz',
+            ['rviz/robot_config.rviz']
+        ),
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='MinhDuc Tran',
     maintainer_email='ductm.tran@gmail.com',
-    description='TODO: Package description',
+    description='ROS 2 palletizing robot learning project',
     license='TODO: License declaration',
     extras_require={
         'test': [
